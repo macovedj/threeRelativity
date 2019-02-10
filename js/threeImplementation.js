038680340cb2e68dbd8d3bd9453b50363b4edc33
macovedj <djmacovei@gmail.com>
2019-02-10 17:17:49 -0500

    add motion

--- js/threeImplementation.js
+++ js/threeImplementation.js
@@ -7,10 +7,33 @@ let time = 0;
 let travellerSpeed = 0;
 let gamma = 1;
 let movingEndpoint = .5;
+let playing = true;
 
 let sliderNode = document.querySelector('#speedSlider');          
 let movingTimeNode = document.querySelector('#movingTime');          
 let stationaryTimeNode = document.querySelector('#stationaryTime');          
+let resetButtonNode = document.querySelector('#resetButton');          
+// let startButtonNode = document.querySelector('#startButton');          
+let pauseButtonNode = document.querySelector('#pauseButton');          
+let labelValue = document.querySelector('#labelValue');
+
+function resetHandler() {
+	rTime = 0;
+	time = 0;
+	movingLine.position.x = 0;
+	playing = false;
+}
+
+// function startHandler() {
+// 	console.log('start happened')
+// 	lastTime = performance.now;
+// 	playing = true;
+// }
+
+function pauseHandler() {
+	console.log('pause happened')
+	playing = false;
+}
 
 function computeGamma() { return 1/Math.sqrt(1 - Math.pow(travellerSpeed, 2)); }
 
@@ -19,22 +42,25 @@ const renderTimers = (time, rTime) => {
 	stationaryTimeNode.innerText = moment.duration(time).format('mm:ss:SS', { trim: false });
 }
 
+// startButtonNode.addEventListener("click", startHandler);
+pauseButtonNode.addEventListener("click", pauseHandler);
+resetButtonNode.addEventListener("click", resetHandler);
+
 sliderNode.addEventListener("input", ev => {
 	travellerSpeed = ev.target.value;
+	labelValue.innerHTML = sliderNode.value;
 	gamma = computeGamma();
-	movingEndpoint = .5 / gamma;
+	movingLine.geometry.vertices[1].x = .5 / gamma;
+	movingLine.scale.set(1 / gamma, 1, 1);
 })
 
 function onMouseWheel(event) {
-
 	event.preventDefault();
-
 	camera.position.x += event.deltaX * 0.005;
 
 	// prevent scrolling beyond a min/max value
 
 	camera.position.clampScalar(0, 100);
-
 }
 
 var scene = new THREE.Scene();
@@ -63,25 +89,23 @@ stationaryGeometry.vertices.push(new THREE.Vector3( 100, .5, 0) );
 
 const linesMaterial = new THREE.LineBasicMaterial( { color: 0x9f8ec2 } );
 
-for ( var i = 0; i <=100; i++ ) {
+for ( var i = 0; i <= 200; i++ ) {
 
 	var lightSecondMark = new THREE.Geometry();
+
 	lightSecondMark.vertices.push(new THREE.Vector3( i * .5, .4, 0));
 	lightSecondMark.vertices.push(new THREE.Vector3( i * .5, .6, 0));
 	var lightSecondLine = new THREE.LineSegments( lightSecondMark, linesMaterial );
 	scene.add( lightSecondLine );
+	// scene.add( label );
 
 }
-
-// movingLightSecond.vertices.push(new THREE.Vector3( 0, 1, 0) );
-// movingLightSecond.vertices.push(new THREE.Vector3( .5 / gamma, 1, 0) );
 
 // create a material, color, or image texture
 var stationaryMaterial = new THREE.LineBasicMaterial( { color: 0x9f8ec2 } );
 var movingMaterial = new THREE.LineBasicMaterial( { color: 0x5f9ea0 } );
 
 var stationaryLine = new THREE.Line( stationaryGeometry, stationaryMaterial );
-var movingLightSecond = new THREE.Geometry();
 movingLightSecond.verticesNeedUpdate = true
 movingLightSecond.vertices.push(new THREE.Vector3( 0, 1, 0) );
 movingLightSecond.vertices.push(new THREE.Vector3( movingEndpoint, 1, 0) );
@@ -94,18 +118,17 @@ scene.add(stationaryLine);
 
 // update logic
 var update = function() {
-	const now = performance.now();
-	const sinceLast = now - lastTime;
+	if (playing) {
+		const now = performance.now();
+		const sinceLast = now - lastTime;
 
-	time += sinceLast;
-	rTime += sinceLast / gamma;
-	renderTimers( time, rTime );
-	lastTime = now;
-
-	movingLine.geometry.vertices[1].x = movingEndpoint;
-	
-	console.log(movingLine.geometry.vertices)
-}
+		time += sinceLast;
+		rTime += sinceLast / gamma;
+		renderTimers( time, rTime );
+		lastTime = now;
+		movingLine.position.x += travellerSpeed * sinceLast * .0005
+	}
+}	
 
 // draw Scene
 var render = function() {
@@ -114,7 +137,7 @@ var render = function() {
 
 // run game loop (update, render, repeate)
 var GameLoop = function() {
-	update();
+  update();
   render();
   requestAnimationFrame( GameLoop );
 };
