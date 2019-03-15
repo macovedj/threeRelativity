6a9574d674212d1b9577678643fcc4be312253a0
macovedj <djmacovei@gmail.com>
2019-03-14 23:10:02 -0400

    initial attempt

--- js/threeImplementation.js
+++ js/threeImplementation.js
@@ -17,6 +17,8 @@ let movingPhotonTimeNode = document.querySelector('#movingPhotonTime');
 let stationaryTimeNode = document.querySelector('#stationaryTime');          
 let resetButtonNode = document.querySelector('#resetButton');          
 let startButtonNode = document.querySelector('#startButton');          
+let recenterXButtonNode = document.querySelector('#recenterXButton');          
+let recenterYButtonNode = document.querySelector('#recenterYButton');          
 let labelValue = document.querySelector('#labelValue');
 let gammaLabel = document.querySelector('#gammaLabel');          
 let pauseInput = document.querySelector('#pauseTime');
@@ -24,25 +26,28 @@ let pauseInput = document.querySelector('#pauseTime');
 startButtonNode.addEventListener("click", startHandler);
 resetButtonNode.addEventListener("click", resetHandler);
 pauseInput.addEventListener("input", pauseInputHandler);
+recenterXButtonNode.addEventListener("click", recenterXHandler);
+recenterYButtonNode.addEventListener("click", recenterYHandler);
 
 function resetHandler() {
 	sliderNode.value = 0;
 	gamma = 1;
 	relativisticPhotonTime = 0;
 	time = 0;
-	speedOfTraveller = 0;
+	travellerSpeed = 0;
 	movingPhotonTimeNode.innerHTML = "00:00:00";
 	stationaryTimeNode.innerHTML = "00:00:00";
 	labelValue.innerHTML = "0";
 	gammaLabel.innerHTML = "1";
-	movingLine.position.x = 0;
-	photon.position.x = 0;
+	movingLineA.position.x = 0;
+	photonA.position.x = 0;
 	movingSprites.forEach(sprite => scene.remove(sprite));
 	movingLightSecondLines.forEach(line => scene.remove(line));
 	movingLightSecondLines = [];
 	movingSprites = [];
+	pauseTime = pauseInput.value;
 	for (var i = 0; i <= 200; i++) {
-		createMovingLightSecond(i, gamma);
+		createMovingLightSecondA(i, gamma);
 	}
 	movingLightSecondLines.forEach(line => scene.add(line))
 	movingSprites.forEach(sprite => scene.add(sprite))
@@ -67,13 +72,22 @@ function pauseInputHandler() {
 function onMouseWheel(event) {
 	event.preventDefault();
 	camera.position.x += event.deltaX * 0.005;
+	camera.position.y += event.deltaY * 0.005;
 
 	// prevent scrolling beyond a min/max value
 
-	camera.position.clampScalar(0, 1000);
+	camera.position.clampScalar(-1000, 1000);
 }
 
-function createStationaryLightSecond(i) {
+function recenterXHandler() {
+	camera.position.x = 0;
+}
+
+function recenterYHandler() {
+	camera.position.y = 0;
+}
+
+function createStationaryLightSecondA(i) {
 	var lightSecondMark = new THREE.Geometry();
 	lightSecondMark.vertices.push(new THREE.Vector3( i * 1.5, .9, 0));
 	lightSecondMark.vertices.push(new THREE.Vector3( i * 1.5, 1.1, 0));
@@ -98,7 +112,32 @@ function createStationaryLightSecond(i) {
 	scene.add(sprite);
 }
 
-function createMovingLightSecond(i, gamma) {
+function createStationaryLightSecondB(i) {
+	var lightSecondMark = new THREE.Geometry();
+	lightSecondMark.vertices.push(new THREE.Vector3(i * 1.5, -.6, 0));
+	lightSecondMark.vertices.push(new THREE.Vector3(i * 1.5, -.4, 0));
+	var lightSecondLine = new THREE.LineSegments(lightSecondMark, movingLinesMaterial);
+	let canvas = document.createElement('canvas');
+	canvas.width = 256;
+	canvas.height = 256;
+	var ctx = canvas.getContext("2d");
+	ctx.font = "22pt Arial";
+	ctx.fillStyle = "#5f9ea0";
+	ctx.textAlign = "center";
+	ctx.fillText(i, 128, 44);
+	var tex = new THREE.Texture(canvas);
+	tex.needsUpdate = true;
+	var spriteMat = new THREE.SpriteMaterial({
+		map: tex
+	});
+	var sprite = new THREE.Sprite(spriteMat);
+	sprite.position.x = i * 1.5;
+	sprite.position.y = -1.1;
+	scene.add(lightSecondLine);
+	scene.add(sprite);
+}
+
+function createMovingLightSecondA(i, gamma) {
 	var movingLightSecondMark = new THREE.Geometry();
 	movingLightSecondMark.vertices.push(new THREE.Vector3( i * 1.5 / gamma, 1.4, 0));
 	movingLightSecondMark.vertices.push(new THREE.Vector3( i * 1.5 / gamma, 1.6, 0));
@@ -120,6 +159,31 @@ function createMovingLightSecond(i, gamma) {
 	var sprite = new THREE.Sprite(spriteMat);
 	sprite.position.x = i * 1.5 / gamma;
 	sprite.position.y = .9;
+	movingSprites.push(sprite);
+}
+
+function createMovingLightSecondB(i, gamma) {
+	var movingLightSecondMark = new THREE.Geometry();
+	movingLightSecondMark.vertices.push(new THREE.Vector3(i * 1.5 / gamma, -1.1, 0));
+	movingLightSecondMark.vertices.push(new THREE.Vector3(i * 1.5 / gamma, -.9, 0));
+	var movingLightSecondLine = new THREE.LineSegments(movingLightSecondMark, linesMaterial);
+	movingLightSecondLines.push(movingLightSecondLine);
+	let canvas = document.createElement('canvas');
+	canvas.width = 256;
+	canvas.height = 256;
+	var ctx = canvas.getContext("2d");
+	ctx.font = "22pt Arial";
+	ctx.fillStyle = "#9f8ec2";
+	ctx.textAlign = "center";
+	ctx.fillText(i, 128, 44);
+	var tex = new THREE.Texture(canvas);
+	tex.needsUpdate = true;
+	var spriteMat = new THREE.SpriteMaterial({
+		map: tex
+	});
+	var sprite = new THREE.Sprite(spriteMat);
+	sprite.position.x = i * 1.5 / gamma;
+	sprite.position.y = -1.6;
 	movingSprites.push(sprite);
 }
 
@@ -154,26 +218,37 @@ sliderNode.addEventListener("input", ev => {
 	gamma = computeGamma();
 	gammaLabel.innerHTML = gamma;
 	movingLightSecondLines.forEach(line => line.scale.set(1 / gamma, 1, 1)) 
-	movingSprites.forEach((sprite,i) => sprite.position.x = movingSprites[0].position.x + i * 1.5 / gamma);
+	movingSprites.forEach((sprite, i) => sprite.position.x = movingSprites[0].position.x + i * 1.5 / gamma);
 })
 
 // Create each measurement line and photon
-var stationaryGeometry = new THREE.Geometry();
-var movingGeometry = new THREE.Geometry();
-var photonGeometry = new THREE.PlaneGeometry(.05,.05,.05);
+var stationaryGeometryA = new THREE.Geometry();
+var movingGeometryA = new THREE.Geometry();
+var photonGeometryA = new THREE.PlaneGeometry(.05,.05,.05);
 
-stationaryGeometry.vertices.push(new THREE.Vector3( -10, 1, 0) );
-stationaryGeometry.vertices.push(new THREE.Vector3( 1000, 1, 0) );
-movingGeometry.vertices.push(new THREE.Vector3( -10, 1.5, 0) );
-movingGeometry.vertices.push(new THREE.Vector3( 1000, 1.5, 0) );
+var stationaryGeometryB = new THREE.Geometry();
+var movingGeometryB = new THREE.Geometry();
+var photonGeometryB = new THREE.PlaneGeometry(.05, .05, .05);
+
+stationaryGeometryA.vertices.push(new THREE.Vector3( -1000, 1, 0) );
+stationaryGeometryA.vertices.push(new THREE.Vector3( 1000, 1, 0) );
+movingGeometryA.vertices.push(new THREE.Vector3( -1000, 1.5, 0) );
+movingGeometryA.vertices.push(new THREE.Vector3( 1000, 1.5, 0) );
+
+stationaryGeometryB.vertices.push(new THREE.Vector3(-1000, -1, 0));
+stationaryGeometryB.vertices.push(new THREE.Vector3(1000, -1, 0));
+movingGeometryB.vertices.push(new THREE.Vector3(-1000, -.5, 0));
+movingGeometryB.vertices.push(new THREE.Vector3(1000, -.5, 0));
 
 const linesMaterial = new THREE.LineBasicMaterial( { color: 0x9f8ec2 } );
 const movingLinesMaterial = new THREE.LineBasicMaterial( { color: 0x5f9ea0 } );
 
 // Create tick marks on each line
 for ( var i = 0; i <= 200; i++ ) {
-	createStationaryLightSecond(i);
-	createMovingLightSecond(i, gamma);
+	createStationaryLightSecondA(i);
+	createMovingLightSecondA(i, gamma);
+	createStationaryLightSecondB(i);
+	createMovingLightSecondB(i, gamma);
 }
 movingLightSecondLines.forEach(mark => scene.add(mark));
 movingSprites.forEach(sprite => scene.add(sprite));
@@ -183,15 +258,24 @@ var stationaryMaterial = new THREE.LineBasicMaterial( { color: 0x9f8ec2 } );
 var movingMaterial = new THREE.LineBasicMaterial( { color: 0x5f9ea0 } );
 const photonMaterial = new THREE.MeshBasicMaterial( { color: 0xffe623, side: THREE.DoubleSide } );
 
-var stationaryLine = new THREE.Line( stationaryGeometry, stationaryMaterial );
-var movingLine = new THREE.Line( movingGeometry, movingMaterial );
-var photon = new THREE.Mesh( photonGeometry, photonMaterial );
-photon.position.y = 2
+var stationaryLineA = new THREE.Line( stationaryGeometryA, stationaryMaterial );
+var movingLineA = new THREE.Line( movingGeometryA, movingMaterial );
+var photonA = new THREE.Mesh( photonGeometryA, photonMaterial );
+photonA.position.y = 2;
+
+var stationaryLineB = new THREE.Line(stationaryGeometryB, stationaryMaterial);
+var movingLineB = new THREE.Line(movingGeometryB, movingMaterial);
+var photonB = new THREE.Mesh(photonGeometryB, photonMaterial);
+photonB.position.y = 0;
 camera.position.z = 3;
 
-scene.add(stationaryLine);
-scene.add(movingLine);
-scene.add(photon);
+scene.add(stationaryLineA);
+scene.add(movingLineA);
+scene.add(photonA);
+
+scene.add(stationaryLineB);
+scene.add(movingLineB);
+scene.add(photonB);
 
 // update logic
 var update = function() {
@@ -210,7 +294,7 @@ var update = function() {
 		lastTime = now;
 		movingLightSecondLines.forEach(line =>  line.position.x += travellerSpeed * sinceLast * .0015);
 		movingSprites.forEach(sprite =>  sprite.position.x += travellerSpeed * sinceLast * .0015);
-		photon.position.x += 1.5 * sinceLast / 1000
+		photonA.position.x += 1.5 * sinceLast / 1000
 	}
 }	
 
